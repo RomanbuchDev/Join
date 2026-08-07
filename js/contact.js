2c7975861c8f6070b748f7956e9191e8576ca837
Timo Indlekofer <timo.indlekofer@web.de>
2026-08-07 17:05:28 +0200

    feat/5-contacts-basic-structure (mobile version)
    feat: issue 5: contacts.html
    - add function openDialogAddContact() to add contact button
    - add dialog for add contact button
    
    feat: issue 5: contacts.css
    - update css class main-container
    - add css class footer-link
    - add css classes for dialog for add contact button
    
    feat: issue 5: contacts.js
    - add variable dialogBoxAddContact
    - add function openDialogAddContact()
    - add function closeDialogAddContact()
    
    feat: issue 5: icons
    - add icon for check icon
    - add icon for email icon
    - add icon for person icon
    - add icon for phone icon

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,20 @@
 // JavaScript file for contact page
+
+// Variables:
+
+const dialogBoxAddContact = document.getElementById('add-contact-dialog');
+
+
+
+
+
+// Functions:
+
+function openDialogAddContact() {
+    dialogBoxAddContact.showModal();
+}
+
+
+function closeDialogAddContact() {
+    dialogBoxAddContact.close();
+}
