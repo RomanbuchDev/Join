c10c89419bd6d50b29b427c78cefa020a2eebb77
Timo Indlekofer <timo.indlekofer@web.de>
2026-08-08 16:27:41 +0200

    feat/5-contacts-basic-structure
    feat: issue 5: contacts.html
    - add id to contact list
    - add function showContactDetails() to contact button
    - add contact details
    - add ids to input fields in dialog
    - add delete button in dialog
    - add save button in dialog
    - add message for contact successfully created in dialog
    
    feat: issue 5: contacts.css
    - update css class name from contact-add-button to contact-menu
    - add css classes for contact details
    - update all css classes for add-contact
    - update css class name from add-contact-title-underline to title-underline
    - update css class add-contact-footer
    - update css class name from create-contact to create-contact-data
    - add css classes for delete button, save button and contact created message
    - add css classes show and hidden
    
    feat: issue 5: contacts.js
    - add variables contactList, contactDetails, iconAddContact, iconMoreMenu, addContactForm, contactName, contactEmail, contactPhone and messageContactCreated
    - add array allContacts for test
    - add functions showContactDetails(), backToContactList(), hideToastMessageContactCreated(), showToastMessageContactCreated(), resetFormInputs() and createContact()

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,48 @@
 // JavaScript file for contact page
 
 // Variables:
+const contactList = document.getElementById('contact-list');
+const contactDetails = document.getElementById('contact-details');
+const iconAddContact = document.getElementById('add-contact-icon');
+const iconMoreMenu = document.getElementById('more-menu-icon');
 
 const dialogBoxAddContact = document.getElementById('add-contact-dialog');
+const addContactForm = document.getElementById('add-contact-dialog').querySelector('form');
+const contactName = document.getElementById('input-contact-name-dialog');
+const contactEmail = document.getElementById('input-contact-email-dialog');
+const contactPhone = document.getElementById('input-contact-phone-dialog');
+const messageContactCreated = document.getElementById('toast-message-contact-created');
 
+
+
+
+
+// Database (for test only):
+
+let allContacts = [];
 
 
 
 
 // Functions:
+
+function showContactDetails() {
+    contactList.classList.add('hidden');
+    contactDetails.classList.remove('hidden');
+
+    iconAddContact.classList.replace('show', 'hidden');
+    iconMoreMenu.classList.replace('hidden', 'show');
+}
+
+
+function backToContactList() {
+    contactList.classList.remove('hidden');
+    contactDetails.classList.add('hidden');
+    
+    iconAddContact.classList.replace('hidden', 'show');
+    iconMoreMenu.classList.replace('show', 'hidden');
+}
+
 
 function openDialogAddContact() {
     dialogBoxAddContact.showModal();
@@ -17,4 +51,33 @@ function openDialogAddContact() {
 
 function closeDialogAddContact() {
     dialogBoxAddContact.close();
+}
+
+
+function hideToastMessageContactCreated() {
+    messageContactCreated.classList.remove('show');
+}
+
+
+function showToastMessageContactCreated() {
+    messageContactCreated.classList.add('show');
+    setTimeout(hideToastMessageContactCreated, 3000);
+}
+
+
+function resetFormInputs() {
+    addContactForm.reset();
+}
+
+
+function createContact() {
+    allContacts.push({
+        "name": contactName.value,
+        "email": contactEmail.value,
+        "phone": contactPhone.value
+    });
+    resetFormInputs();
+    showToastMessageContactCreated();
+
+    console.log(allContacts);
 }
