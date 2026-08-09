a625dfe5a47ebf11f0de077e512cd5d9fb7962de
Timo Indlekofer <timo.indlekofer@web.de>
2026-08-09 16:20:47 +0200

    feat/5-contacts-basic-structure
    feat: issue 5: contacts.html
    - add function init() to body as onload
    - add id to letter group
    - update css class name from contact-grid to contact-grid-container
    - change position of button for dialog
    - update css class name from add-contact to add-contact-container
    - add contact option menu
    
    feat: issue 5: contacts.css
    - update css class names
    - add css classes for contact option menu
    - update css class show
    
    feat: issue 5: contacts.jss
    - remove variables iconAddContact and iconMoreMenu
    - add variables contactListGrid, contactOptionMenu and contactOptionMenuOverlay
    - add array with contacts
    - add functions init(), getContactShortcut(), renderContactList(), toggleContactPageView(), toggleMobileContactOptions() and closeMobileContactOptions()
    - update functions showContactDetails() and backToContactList()
    - add template functions getContactTemplate() and getContactDetailsTemplate()
    
    feat: issues 5: icons
    - add icons for contact option, delete and edit

--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,10 @@
 
 // Variables:
 const contactList = document.getElementById('contact-list');
+const contactListGrid = document.getElementById('contact-grid');
 const contactDetails = document.getElementById('contact-details');
-const iconAddContact = document.getElementById('add-contact-icon');
-const iconMoreMenu = document.getElementById('more-menu-icon');
+const contactOptionMenu = document.getElementById('contact-options');
+const contactOptionMenuOverlay = document.getElementById('contact-options-overlay');
 
 const dialogBoxAddContact = document.getElementById('add-contact-dialog');
 const addContactForm = document.getElementById('add-contact-dialog').querySelector('form');
@@ -16,32 +17,138 @@ const messageContactCreated = document.getElementById('toast-message-contact-cre
 
 
 
-
 // Database (for test only):
 
-let allContacts = [];
+let allContacts = [
+    {
+        "id": 1,
+        "name": "Anton Mayer",
+        "email": "antonm@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 2,
+        "name": "Anja Schulz",
+        "email": "schulz@hotmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 3,
+        "name": "Benedikt Ziegler",
+        "email": "benedikt@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 4,
+        "name": "David Eisenberg",
+        "email": "davidberg@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 5,
+        "name": "Eva Fischer",
+        "email": "eva@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 6,
+        "name": "Emmanuel Mauer",
+        "email": "emmanuelma@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 7,
+        "name": "Marcel Bauer",
+        "email": "bauer@gmail.com",
+        "phone": "+49 123 4567890"
+    },
+    {
+        "id": 8,
+        "name": "Tatjana Wolf",
+        "email": "wolf@gmail.com",
+        "phone": "+49 123 4567890"
+    }
+];
+
 
 
 
 
 // Functions:
 
-function showContactDetails() {
-    contactList.classList.add('hidden');
-    contactDetails.classList.remove('hidden');
+function init() {
+    renderContactList();
+}
 
-    iconAddContact.classList.replace('show', 'hidden');
-    iconMoreMenu.classList.replace('hidden', 'show');
+
+function getContactShortcut(contactData) {
+    const contactNameParts = contactData.name.split(" ");
+   
+    const firstNameLetter = contactNameParts[0][0];
+    const LastNameLetter = contactNameParts[1][0];
+
+    const contact = allContacts.find(name => name.id === contactData.id);
+
+    if (contact) {
+        contact.shortcut = firstNameLetter + LastNameLetter;
+    }
+}
+
+
+function renderContactList() {
+    contactListGrid.innerHTML = '';
+
+    let contactListData = [];
+
+    for (let i = 0; i < allContacts.length; i++) {
+        const contactData = allContacts[i];
+        getContactShortcut(contactData);
+        contactListData += getContactTemplate(contactData);         
+    }
+    contactListGrid.innerHTML = contactListData;
+}
+
+
+function showContactDetails(contactID) {
+    toggleContactPageView(contactDetails, contactList);
+
+    contactDetails.innerHTML = '';
+
+    const contact = allContacts.find(name => name.id === contactID);
+    const contactDetailsData = getContactDetailsTemplate(contact);
+
+    contactDetails.innerHTML = contactDetailsData;
 }
 
 
 function backToContactList() {
-    contactList.classList.remove('hidden');
-    contactDetails.classList.add('hidden');
-    
-    iconAddContact.classList.replace('hidden', 'show');
-    iconMoreMenu.classList.replace('show', 'hidden');
+    toggleContactPageView(contactList, contactDetails);
 }
+
+
+function toggleContactPageView(show, hide) {
+    show.classList.remove('hidden');
+    hide.classList.add('hidden');
+}
+
+
+function toggleMobileContactOptions() {
+    if (contactOptionMenu && contactOptionMenuOverlay) {
+        contactOptionMenu.classList.add('show');
+        contactOptionMenuOverlay.classList.add('show');
+    }
+}
+
+
+function closeMobileContactOptions() {
+    contactOptionMenu.classList.remove('show');
+    contactOptionMenuOverlay.classList.remove('show');
+}
+
+
+
+
+// Dialog
 
 
 function openDialogAddContact() {
@@ -80,4 +187,57 @@ function createContact() {
     showToastMessageContactCreated();
 
     console.log(allContacts);
+}
+
+
+
+
+
+// Templates:
+
+
+function getContactTemplate(contactData) {
+    return `<button type="button" class="contact-card" onclick="showContactDetails(${contactData.id})">
+            <span class="contact-shortcut">${contactData.shortcut}</span>
+            <div>
+              <span class="contact-name">${contactData.name}</span>
+              <span class="contact-e-mail">${contactData.email}</span>
+            </div>
+          </button>`
+}
+
+
+function getContactDetailsTemplate(contactData) {
+    return `<div class="contact-details-header-container">
+        <div class="contact-details-title-container">
+          <span class="contact-details-title">Contacts</span>
+          <span class="contact-details-subtitle">Better with a team</span>
+          <span class="title-underline"></span>
+        </div>
+        <button type="button" class="contact-details-back-button" onclick="backToContactList()">&#x1F860;</button>
+      </div>
+
+      <!-- Contact details main -->
+      <div class="contact-details-name-container">
+        <span class="contact-details-shortcut">${contactData.shortcut}</span>
+        <span class="contact-details-name">${contactData.name}</span>
+      </div>
+
+      <!-- Contact details information -->
+      <div>
+        <div class="contact-details-information-container">
+          <h3>Contact information</h3>
+        </div>
+        <div class="contact-details-contact-data">
+          <h4>Email</h4>
+          <span class="contact-e-mail">${contactData.email}</span>
+          <h4>Phone</h4>
+          <span>${contactData.phone}</span>
+        </div>
+      </div>
+
+      <!-- Contact details option menu button -->
+      <button class="contact-menu" onclick="toggleMobileContactOptions()">
+        <img src="../assets/icons/contacts/contact_options_icon.png" alt="Contact options button mobile">
+      </button>`
 }
