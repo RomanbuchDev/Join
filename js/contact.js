4bbff3a58973e2a70a7a8ad674b36334dde99677
Timo Indlekofer <timo.indlekofer@web.de>
2026-08-14 12:41:33 +0200

    feat/5-contacts-basic-structure

--- js/contact.js
+++ js/contact.js
@@ -1,214 +1,282 @@
 // JavaScript file for contact page
 
 // Variables:
-const contactList = document.getElementById('contact-list');
-const contactListGrid = document.getElementById('contact-grid');
-const contactDetails = document.getElementById('contact-details');
-const contactOptionMenu = document.getElementById('contact-options');
-const contactOptionMenuOverlay = document.getElementById('contact-options-overlay');
+const contactList = document.getElementById("contact-list");
+const contactListGrid = document.getElementById("contact-grid");
+const contactDetails = document.getElementById("contact-details");
+const contactOptionMenu = document.getElementById("contact-options");
+const contactOptionMenuOverlay = document.getElementById("contact-options-overlay");
 
-const dialogBoxAddContact = document.getElementById('add-contact-dialog');
-const addContactForm = document.getElementById('add-contact-dialog').querySelector('form');
-const contactName = document.getElementById('input-contact-name-dialog');
-const contactEmail = document.getElementById('input-contact-email-dialog');
-const contactPhone = document.getElementById('input-contact-phone-dialog');
-const messageContactCreated = document.getElementById('toast-message-contact-created');
-
-
+const dialogBoxAddContact = document.getElementById("add-contact-dialog");
+const addContactForm = document.getElementById("add-contact-dialog").querySelector("form");
+const contactName = document.getElementById("input-contact-name-dialog");
+const contactEmail = document.getElementById("input-contact-email-dialog");
+const contactPhone = document.getElementById("input-contact-phone-dialog");
+const messageContactCreated = document.getElementById("toast-message-contact-created");
 
 
 // Database (for test only):
 
+
 let allContacts = [
-    {
-        "id": 1,
-        "name": "Anton Mayer",
-        "email": "antonm@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 2,
-        "name": "Anja Schulz",
-        "email": "schulz@hotmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 3,
-        "name": "Benedikt Ziegler",
-        "email": "benedikt@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 4,
-        "name": "David Eisenberg",
-        "email": "davidberg@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 5,
-        "name": "Eva Fischer",
-        "email": "eva@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 6,
-        "name": "Emmanuel Mauer",
-        "email": "emmanuelma@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 7,
-        "name": "Marcel Bauer",
-        "email": "bauer@gmail.com",
-        "phone": "+49 123 4567890"
-    },
-    {
-        "id": 8,
-        "name": "Tatjana Wolf",
-        "email": "wolf@gmail.com",
-        "phone": "+49 123 4567890"
-    }
+  {
+    id: 1,
+    name: "Anton Mayer",
+    email: "antonm@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 2,
+    name: "Anja Schulz",
+    email: "schulz@hotmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 3,
+    name: "Benedikt Ziegler",
+    email: "benedikt@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 4,
+    name: "David Eisenberg",
+    email: "davidberg@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 5,
+    name: "Eva Fischer",
+    email: "eva@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 6,
+    name: "Emmanuel Mauer",
+    email: "emmanuelma@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 7,
+    name: "Marcel Bauer",
+    email: "bauer@gmail.com",
+    phone: "+49 123 4567890",
+  },
+  {
+    id: 8,
+    name: "Tatjana Wolf",
+    email: "wolf@gmail.com",
+    phone: "+49 123 4567890",
+  },
 ];
 
 
-
+let contactListCategory = [];
 
 
 // Functions:
 
 function init() {
-    renderContactList();
+  renderContactList();
+}
+
+
+function checkFirstLetterOfContact(contactData) {
+  const firstLetter = contactData.name[0];
+  const checkFirstLetter = contactListCategory.includes(firstLetter);
+
+  if (checkFirstLetter !== true) {
+    contactListCategory.push(firstLetter);
+  } else {
+    return;
+  }
+}
+
+
+function renderLetterCategories() {
+  contactList.innerHTML = "";
+
+  let letterCategories = [];
+
+  for (let i = 0; i < contactListCategory.length; i++) {
+    const contactListCategoryLetters = contactListCategory[i];
+    letterCategories += getLetterCategoryTemplate(contactListCategoryLetters);
+  }
+  contactList.innerHTML = letterCategories;
+}
+
+
+function assignContactToLetterCategory(firstLetter) {
+  const contactCategoryGrid = document.getElementById(
+    "contact-grid-" + firstLetter,
+  );
+
+  let contactListData = "";
+
+  for (let i = 0; i < allContacts.length; i++) {
+    const contactData = allContacts[i];
+
+    if (firstLetter === contactData.name[0]) {
+      contactListData += getContactTemplate(contactData);
+    }
+  }
+  contactCategoryGrid.innerHTML = contactListData;
+}
+
+
+function setContactIconColor() {
+  for (let i = 0; i < allContacts.length; i++) {
+    const contactData = allContacts[i];
+    const shortcutColor = calculateContactIconColor(contactData.shortcut);
+
+    const contactID = document.getElementById(`contact-${contactData.id}`);
+
+    if (contactID) {
+      contactID.style.setProperty("--background-color", shortcutColor);
+    }
+  }
+}
+
+
+function calculateContactIconColor(contactShortcut) {
+  const correctShortcut = contactShortcut.toUpperCase();
+
+  const firstNameLetter = correctShortcut[0];
+  const LastNameLetter = correctShortcut[1];
+
+  const value1 = firstNameLetter.charCodeAt(0) - 65;
+  const value2 = LastNameLetter.charCodeAt(0) - 65;
+
+  const r = (40 + value1 * 7).toFixed(0);
+  const g = (40 + value2 * 7).toFixed(0);
+  const b = (40 + (25 - value2) * 7).toFixed(0);
+
+  return `rgba(${r}, ${g}, ${b}, 1)`;
 }
 
 
 function getContactShortcut(contactData) {
-    const contactNameParts = contactData.name.split(" ");
-   
-    const firstNameLetter = contactNameParts[0][0];
-    const LastNameLetter = contactNameParts[1][0];
+  const contactNameParts = contactData.name.split(" ");
 
-    const contact = allContacts.find(name => name.id === contactData.id);
+  const firstNameLetter = contactNameParts[0][0];
+  const LastNameLetter = contactNameParts[1][0];
 
-    if (contact) {
-        contact.shortcut = firstNameLetter + LastNameLetter;
-    }
+  const contact = allContacts.find((name) => name.id === contactData.id);
+
+  if (contact) {
+    contact.shortcut = firstNameLetter + LastNameLetter;
+  }
 }
 
 
 function renderContactList() {
-    contactListGrid.innerHTML = '';
+  let contactListData = [];
 
-    let contactListData = [];
+  for (let i = 0; i < allContacts.length; i++) {
+    const contactData = allContacts[i];
+    getContactShortcut(contactData);
+    checkFirstLetterOfContact(contactData);
+  }
+  renderLetterCategories();
 
-    for (let i = 0; i < allContacts.length; i++) {
-        const contactData = allContacts[i];
-        getContactShortcut(contactData);
-        contactListData += getContactTemplate(contactData);         
-    }
-    contactListGrid.innerHTML = contactListData;
+  for (let i = 0; i < contactListCategory.length; i++) {
+    assignContactToLetterCategory(contactListCategory[i]);
+  }
+  setContactIconColor();
 }
 
 
 function showContactDetails(contactID) {
-    toggleContactPageView(contactDetails, contactList);
+  toggleContactPageView(contactDetails, contactList);
 
-    contactDetails.innerHTML = '';
+  contactDetails.innerHTML = "";
 
-    const contact = allContacts.find(name => name.id === contactID);
-    const contactDetailsData = getContactDetailsTemplate(contact);
+  const contact = allContacts.find((name) => name.id === contactID);
+  const contactDetailsData = getContactDetailsTemplate(contact);
 
-    contactDetails.innerHTML = contactDetailsData;
+  contactDetails.innerHTML = contactDetailsData;
 }
 
 
 function backToContactList() {
-    toggleContactPageView(contactList, contactDetails);
+  toggleContactPageView(contactList, contactDetails);
 }
 
-
 function toggleContactPageView(show, hide) {
-    show.classList.remove('hidden');
-    hide.classList.add('hidden');
+  show.classList.remove("hidden");
+  hide.classList.add("hidden");
 }
 
 
 function toggleMobileContactOptions() {
-    if (contactOptionMenu && contactOptionMenuOverlay) {
-        contactOptionMenu.classList.add('show');
-        contactOptionMenuOverlay.classList.add('show');
-    }
+  if (contactOptionMenu && contactOptionMenuOverlay) {
+    contactOptionMenu.classList.add("show");
+    contactOptionMenuOverlay.classList.add("show");
+  }
 }
 
 
 function closeMobileContactOptions() {
-    contactOptionMenu.classList.remove('show');
-    contactOptionMenuOverlay.classList.remove('show');
+  contactOptionMenu.classList.remove("show");
+  contactOptionMenuOverlay.classList.remove("show");
 }
-
-
 
 
 // Dialog
 
-
 function openDialogAddContact() {
-    dialogBoxAddContact.showModal();
+  dialogBoxAddContact.showModal();
 }
 
 
 function closeDialogAddContact() {
-    dialogBoxAddContact.close();
+  dialogBoxAddContact.close();
 }
 
 
 function hideToastMessageContactCreated() {
-    messageContactCreated.classList.remove('show');
+  messageContactCreated.classList.remove("show");
 }
 
 
 function showToastMessageContactCreated() {
-    messageContactCreated.classList.add('show');
-    setTimeout(hideToastMessageContactCreated, 3000);
+  messageContactCreated.classList.add("show");
+  setTimeout(hideToastMessageContactCreated, 3000);
 }
 
 
 function resetFormInputs() {
-    addContactForm.reset();
+  addContactForm.reset();
 }
 
 
 function createContact() {
-    allContacts.push({
-        "name": contactName.value,
-        "email": contactEmail.value,
-        "phone": contactPhone.value
-    });
-    resetFormInputs();
-    showToastMessageContactCreated();
+  allContacts.push({
+    name: contactName.value,
+    email: contactEmail.value,
+    phone: contactPhone.value,
+  });
+  resetFormInputs();
+  showToastMessageContactCreated();
 
-    console.log(allContacts);
+  console.log(allContacts);
 }
-
-
-
 
 
 // Templates:
 
-
 function getContactTemplate(contactData) {
-    return `<button type="button" class="contact-card" onclick="showContactDetails(${contactData.id})">
+  return `<button type="button" class="contact-card" id="contact-${contactData.id}" onclick="showContactDetails(${contactData.id})">
             <span class="contact-shortcut">${contactData.shortcut}</span>
             <div>
               <span class="contact-name">${contactData.name}</span>
               <span class="contact-e-mail">${contactData.email}</span>
             </div>
-          </button>`
+          </button>`;
 }
 
 
 function getContactDetailsTemplate(contactData) {
-    return `<div class="contact-details-header-container">
+  return `<div class="contact-details-header-container">
         <div class="contact-details-title-container">
           <span class="contact-details-title">Contacts</span>
           <span class="contact-details-subtitle">Better with a team</span>
@@ -239,5 +307,18 @@ function getContactDetailsTemplate(contactData) {
       <!-- Contact details option menu button -->
       <button class="contact-menu" onclick="toggleMobileContactOptions()">
         <img src="../assets/icons/contacts/contact_options_icon.png" alt="Contact options button mobile">
-      </button>`
+      </button>`;
+}
+
+
+// Test:
+function getLetterCategoryTemplate(letter) {
+  return `<section id="letter-category-${letter}">
+        <div>
+          <h2 class="letter-container">${letter}</h2>
+          <hr class="letter-line">
+        </div>
+        <div class="contact-grid-container" id="contact-grid-${letter}">
+        </div>
+      </section>`;
 }
