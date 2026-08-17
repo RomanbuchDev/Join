7a523f4181139f7a9b5cbcf0a7580323245bed1d
AlexE.Dev <locha8472@t-online.de>
2026-08-17 23:09:40 +0200

    feat: add greeting logic for logged-in users and guests

--- js/greeting-page.js
+++ js/greeting-page.js
@@ -1,3 +1,30 @@
 setTimeout(() => {
   window.location.href = "./summary.html";
 }, 3000);
+
+function initGreeting() {
+  if (!loginStatus()) {
+    return;
+  }
+  greetingUser();
+}
+
+function loginStatus() {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  if (!currentUser) {
+    window.location.href = "../index.html";
+    return false;
+  }
+  return true;
+}
+
+function greetingUser() {
+  let greetingUserData = JSON.parse(localStorage.getItem("currentUser"));
+  if (greetingUserData.isGuest) {
+    document.getElementById("greetingMessage").innerText = "Good morning!";
+    document.getElementById("userName").innerText = "";
+  } else {
+    document.getElementById("greetingMessage").innerText = "Good morning,";
+    document.getElementById("userName").innerText = greetingUserData.name;
+  }
+}
