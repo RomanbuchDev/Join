4060b0524fd40d42edc64e401f9b8e6b3e66c9d1
Roman <rombuc99@gmail.com>
2026-09-02 21:20:49 +0200

    feat: complete summary layout and interactions

--- js/summary.js
+++ js/summary.js
@@ -5,6 +5,7 @@ async function init() {
   getStatusInformation();
 }
 
+
 async function getSummaryData() {
   const response = await fetch("../js/tasks.json");
   const data = await response.json();
@@ -12,24 +13,34 @@ async function getSummaryData() {
   tasks = data;
 }
 
+
 function getStatusInformation() {
-  const statusOptions = ["todo", "in-progress", "await-feedback", "done"];
-  let numberOfToDos = 0;
-  let numberOfInProgress = 0;
-  let numberOfAwaitFeedback = 0;
-  let numberOfDone = 0;
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    numberOfToDos += countStatus(task, "todo");
-    numberOfInProgress += countStatus(task, "in-progress");
-    numberOfAwaitFeedback += countStatus(task, "await-feedback");
-    numberOfDone += countStatus(task, "done");
-  }
-  console.log("Status ToDo: ", numberOfToDos);
-  console.log("Status In Progress: ", numberOfInProgress);
-  console.log("Status Await Feedback: ", numberOfAwaitFeedback);
-  console.log("Status Done: ", numberOfDone);
+  const statusCounts = getStatusCounts();
+  logStatusCounts(statusCounts);
 }
+
+
+function getStatusCounts() {
+  const statusCounts = { todo: 0, inProgress: 0, awaitFeedback: 0, done: 0 };
+
+  for (const task of tasks) {
+    statusCounts.todo += countStatus(task, "todo");
+    statusCounts.inProgress += countStatus(task, "in-progress");
+    statusCounts.awaitFeedback += countStatus(task, "await-feedback");
+    statusCounts.done += countStatus(task, "done");
+  }
+
+  return statusCounts;
+}
+
+
+function logStatusCounts(statusCounts) {
+  console.log("Status ToDo: ", statusCounts.todo);
+  console.log("Status In Progress: ", statusCounts.inProgress);
+  console.log("Status Await Feedback: ", statusCounts.awaitFeedback);
+  console.log("Status Done: ", statusCounts.done);
+}
+
 
 function countStatus(task, status) {
   let counter = 0;
@@ -39,6 +50,7 @@ function countStatus(task, status) {
   return counter;
 }
 
+
 function changeFillIcon(element, action) {
   const circle = element.querySelector(".icon_circle");
   const motif = element.querySelector(".icon_motif");
@@ -46,6 +58,7 @@ function changeFillIcon(element, action) {
   circle.setAttribute("fill", action === "start" ? "white" : "#2A3647");
   motif.setAttribute("fill", action === "start" ? "#2A3647" : "white");
 }
+
 
 function changeStrokeIcon(element, action) {
   const circle = element.querySelector(".icon_circle");
