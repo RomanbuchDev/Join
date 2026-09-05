a6b8d57b32289090ce413c4ad62b450bf2fcb895
AlexE.Dev <locha8472@t-online.de>
2026-09-06 00:17:28 +0200

    feat: add Firebase SDK and initialize the app

---
+++ js/firebase-init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBqzMjMR3tovxxV8gao0Up7JaYSZ4nJwc8",
+    authDomain: "join-7252c.firebaseapp.com",
+    databaseURL: "https://join-7252c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "join-7252c",
+    storageBucket: "join-7252c.firebasestorage.app",
+    messagingSenderId: "103364081217",
+    appId: "1:103364081217:web:76f0c6cf56e2c3acd4f8c0",
+    measurementId: "G-YTX2XYR2VY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
