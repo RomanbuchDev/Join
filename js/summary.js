f2078d0c49ee741663fc0e0e84245f33883fbbfb
Roman <rombuc99@gmail.com>
2026-09-11 01:46:43 +0200

    feat: implement add task logic and styling

--- js/summary.js
+++ js/summary.js
@@ -5,42 +5,36 @@ async function init() {
   getStatusInformation();
 }
 
-
 async function getSummaryData() {
-  const response = await fetch("../js/tasks.json");
+  const response = await fetch(
+    "https://join-7252c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+  );
   const data = await response.json();
-
-  tasks = data;
+  console.log(response.status);
+  console.log(data);
+  tasks = Object.values(data);
 }
-
 
 function getStatusInformation() {
+  const urgentTasks = getStatusUrgent();
+  const summaryList = document.querySelector("#summary_list");
   const statusCounts = getStatusCounts();
-  logStatusCounts(statusCounts);
+  summaryList.innerHTML = "";
+  summaryList.innerHTML = generateSummaryHTM(statusCounts, tasks, urgentTasks);
 }
 
-
 function getStatusCounts() {
-  const statusCounts = { todo: 0, inProgress: 0, awaitFeedback: 0, done: 0 };
+  const statusCounts = { toDo: 0, inProgress: 0, awaitFeedback: 0, done: 0 };
 
   for (const task of tasks) {
-    statusCounts.todo += countStatus(task, "todo");
-    statusCounts.inProgress += countStatus(task, "in-progress");
-    statusCounts.awaitFeedback += countStatus(task, "await-feedback");
+    statusCounts.toDo += countStatus(task, "toDo");
+    statusCounts.inProgress += countStatus(task, "inProgress");
+    statusCounts.awaitFeedback += countStatus(task, "awaitFeedback");
     statusCounts.done += countStatus(task, "done");
   }
 
   return statusCounts;
 }
-
-
-function logStatusCounts(statusCounts) {
-  console.log("Status ToDo: ", statusCounts.todo);
-  console.log("Status In Progress: ", statusCounts.inProgress);
-  console.log("Status Await Feedback: ", statusCounts.awaitFeedback);
-  console.log("Status Done: ", statusCounts.done);
-}
-
 
 function countStatus(task, status) {
   let counter = 0;
@@ -50,6 +44,18 @@ function countStatus(task, status) {
   return counter;
 }
 
+function getStatusUrgent() {
+  let counterUrgent = 0;
+
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    if (task.priority === "urgent") {
+      counterUrgent++;
+    }
+  }
+
+  return counterUrgent;
+}
 
 function changeFillIcon(element, action) {
   const circle = element.querySelector(".icon_circle");
@@ -58,7 +64,6 @@ function changeFillIcon(element, action) {
   circle.setAttribute("fill", action === "start" ? "white" : "#2A3647");
   motif.setAttribute("fill", action === "start" ? "#2A3647" : "white");
 }
-
 
 function changeStrokeIcon(element, action) {
   const circle = element.querySelector(".icon_circle");
